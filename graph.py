5812d9a948bfe23aca4e86ceb5be60ff48741eff
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-10-16 11:29:57 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -22,68 +22,117 @@
 # - Number of Global Begin Lumi Calls:  1
 # - Number of Global Begin Run Calls: 1
 
-from pathlib import Path
+import glob 
 import os
 import matplotlib.pyplot as plt
+import re
+
+#this function allows you to organize files by their decimal numbers
+numbers = re.compile(r'(\d+)')
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
 
 #lists where time and change values are stored
-time = []
+timeA = []
+timeB = []
+timeC = []
+timeD = []
+timeE = []
+timeF = []
 changes = []
 
 #files
-filenames = []
-directory = 'run4' #directory where files are located
+fileA = []
+fileB = []
+fileC = []
+fileD = []
+fileE = []
+fileF = []
+directory = 'run2/'  #directory where files are located
 
-fil = Path(directory).glob('*')
+fil = sorted(glob.glob(directory + '*.txt'), key=numericalSort) #the glob library allows extracting the files from a specific directory and the sorted function organizes the files
 for file in fil:
-        if str(file).startswith('run4/log_DeltaChordSimple_'):
-                filenames.append(file)
+    if file.startswith('run2/log_RusRoEcalGamma_'):
+        fileA.append(file)
+    if file.startswith('run2/log_RusRoHcalGamma_'):
+        fileB.append(file)
+    if file.startswith('run2/log_RusRoMuonIronGamma_'):
+        fileC.append(file)
+    if file.startswith('run2/log_RusRoEcalNeutron_'):
+        fileD.append(file)
+    if file.startswith('run2/log_RusRoHcalNeutron_'):
+        fileE.append(file)
+    if file.startswith('run2/log_RusRoMuonIronNeutron_'):
+        fileF.append(file)
 
-#reading multiple files through one function
+#Files
 def files(filename):
-        try:
-                with open(filename) as f_obj:
-                        for line in f_obj:
-                                if line.startswith(' - Total loop:'):
-                                        one = line.find('  ')
-                                        #two = line.find('s')
-                                        a = line[one+2:]
-        except:
-                msg = filename + " file does not exist "
-                print(msg)
-        return time.append(float(a))
+	try:
+		with open(filename) as f_obj:
+			for line in f_obj:
+				if line.startswith(' - Total loop:'):
+					one = line.find('  ')
+					#two = line.find('s')	
+					a = line[one+2:]
+					#print(a)
+	except:
+		msg = filename + " file does not exist "
+		print(msg)
+	return float(a)
 
 #the for loop executes each of the files in the function
-for filename in filenames:
-        files(filename)
-
-#list of  the file names
-names = []
-for files in os.listdir(directory):
-        if files.startswith('log_DeltaChordSimple'):
-                names.append(files)
+for A,B,C,D,E,F in zip(fileA,fileB,fileC,fileD,fileE,fileF):
+    timeA.append(files(A))
+    timeB.append(files(B))
+    timeC.append(files(C))
+    timeD.append(files(D))
+    timeE.append(files(E))
+    timeF.append(files(F))
 
 #in this loop the value of the changes are extracted
-for value in names:
-        th = value.find('e_')
-        fr = value.find('x')
-        b = value[th+2:fr-2]
-        changes.append(float(b))
+for value in fileA:
+	th = value.find('a_')
+	fr = value.find('x')
+	b = value[th+2:fr-2]
+	changes.append(float(b))
+	#print(value[th])
+
+#print(changes)
+#print(timeA)
+#print(timeB)
+#print(timeC)
+#print(timeD)
+#print(timeE)
+#print(timeF)
 
 #Parameters, if you need to use any of them to produce the graph you just have to uncomment
 #p = 'EnergyThSimple'
 #p = 'DeltaOneStepSimple'
-p = 'DeltaChordSimple'
+#p = 'DeltaChordSimple'
 #p = 'RusRoGammaEnergyLimit'
 #p = 'RusRoNeutronEnergyLimit'
 #p = 'RusRoEcalGamma'
 #p = 'RusRoHcalGamma'
+#p = 'RusRoMuonIronNeutron'
+#p = 'ProductionCut'
+#p = 'Energy Parameters'
+#p = 'Magnetic Parameters'
+p = 'Probability Parameters'
 
 #graphing the behavior of the parameter
-plt.plot(changes,time,marker='o',label='Time Variation')
+plt.plot(changes,timeA,marker='o',label='RusRoEcalGamma Variation')
+plt.plot(changes,timeB,marker='p',label='RusRoHcalGamma Variation')
+plt.plot(changes,timeC,marker='s',label='RusRoMuonIronGamma Variation')
+plt.plot(changes,timeD,marker='P',label='RusRoEcalNeutron Variation')
+plt.plot(changes,timeE,marker='x',label='RusRoHcalNeutron Variation')
+plt.plot(changes,timeF,marker='v',label='RusRoMuonIronNeutron Variation')
+plt.xscale('log')
 plt.xlabel('Changes')
 plt.ylabel('Total Loop')
 plt.title('{}'.format(p), size = 15)
-plt.legend()
-plt.grid()
+plt.legend(ncol=1,bbox_to_anchor=(1.1,0),loc='center right',fontsize=7)
+plt.grid(True)
+plt.tight_layout()
 plt.show()
