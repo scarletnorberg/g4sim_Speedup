689dac224ef5f892d63d211dc19e465f9a593b36
Brian <brian.cruz2@upr.edu>
2022-10-11 18:27:20 -0500

    updated to print a table of data to plot

--- plot_timeloop.py
+++ plot_timeloop.py
@@ -49,6 +49,7 @@ def logline(filenames):
                 b = line[one+2:]      ## returns two times
                 b = float(b)
                 time.append(b)
+                ## time[0] is the run total time loop; time[1] is the CPU total loop
 
     return parameter, time[1]
 
@@ -62,7 +63,6 @@ dict_vals = sorted(files_dict.values(), key=lambda x: x[0])
 
 ### function to get the nominal time of a run ###
 def base_time(parameter,value,amount):
-    #print(parameter, value, amount)
     if amount == 1:     ## single parameter
 
         for bk,bv in baseline_dict.items():
@@ -112,13 +112,16 @@ for k,v in files_dict.items():
     if vals_amount == 1:
         singleparam_values.append(v[0][0][0])
         singleparam_time.append(v[0][1])
-    
+
     else:
         doubleparam_values_x.append(v[0][0][0])
         doubleparam_values_y.append(v[0][0][1])
         doubleparam_time.append(v[0][1])
 
+nominalsingletime = []
+nominaldoubletime = []
 
+nominaltime = 0.0   # place-holder to replace with the nominal time of the chosen run
 ### to get the nominal times of the runs ###
 for k,v in files_dict.items():
     filename = k.replace(directory,"")
@@ -132,16 +135,37 @@ for k,v in files_dict.items():
         nominal_time = base_time(param,v,vals_amount)
         if type(nominal_time) == float:
             paramtoplot_list.append(param)
-            nominalsingletime = [x/nominal_time for x in singleparam_time]
+            nominaltimeratio = [x/nominal_time for x in singleparam_time]
+            nominaltime = nominal_time
+            nominalPV = param_val
 
     else: 
         nominal_time = base_time(param,v,vals_amount)
         if type(nominal_time) == float:
             paramtoplot_list.append(param)
-            nominaldoubletime = [x/nominal_time for x in doubleparam_time]
+            nominaltimeratio = [x/nominal_time for x in doubleparam_time]
+            nominaltime = nominal_time
+            nominalPV = param_val
 
 
+print("The nominal run - ",nominalPV," - time is ", nominaltime)
+print()
 
+for d,t in zip(files_dict.items(),nominaltimeratio):  ## add the nominal time ratio to the corresponding files_dict value
+    d[1].append(t)
+
+## for-loop for table of parameter, values, CPU time and ratio time
+print("||   Parameters_Values   ||   CPU Time   ||   Ratio-Time   ||")
+print("----------------------------------------------------------------------------")
+for k,v in files_dict.items():
+    filename = k.replace(directory,"")
+    param_val = filename.replace("log_","").replace(".txt","")
+    param = param_val.strip("_0.123456789")
+    val = param_val.strip(param)
+
+    print(param_val," || ", v[0][1], " || ",v[1])
+
+### plotting ###        
 for p in paramtoplot_list:
     amount = p.count("_")
     print()
@@ -152,7 +176,7 @@ for p in paramtoplot_list:
         plt.xlabel(p, size=15)
         plt.ylabel("Time loop", size=15)
         plt.scatter(singleparam_values,singleparam_time)
-        #plt.scatter(singleparam_values, nominalsingletime, color='g')
+        ##plt.scatter(singleparam_values, nominalsingletime, color='g')
         plt.savefig(p+"_Timeloop.png")
         plt.close()
 
@@ -161,11 +185,11 @@ for p in paramtoplot_list:
         fig = plt.figure(figsize=(8,6))
         axes3d = Axes3D(fig)
         paramlabels = p.split("_")
-        z = np.linspace(min(nominaldoubletime),max(nominaldoubletime),num=45)
+        z = np.linspace(min(nominaltimeratio),max(nominaltimeratio),num=45)
         plt.xlabel(paramlabels[0], size=15)
         plt.ylabel(paramlabels[1], size=15)
-        axes3d.plot(doubleparam_values_x, doubleparam_values_y, nominaldoubletime)
-        axes3d.scatter3D(doubleparam_values_x, doubleparam_values_y, nominaldoubletime, color=plt.cm.cool(z/max(z)))
+        axes3d.plot(doubleparam_values_x, doubleparam_values_y, nominaltimeratio)
+        axes3d.scatter3D(doubleparam_values_x, doubleparam_values_y, nominaltimeratio, color=plt.cm.cool(z/max(z)))
         axes3d.set_zlabel("Time Loop", size=15)
         plt.savefig(p+"_Timeloop.png", bbox_inches="tight")
         plt.close()
